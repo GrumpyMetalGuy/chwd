7318a15f8e88698bd9f87adc902a8019725172be
Peter Keyzers <peter.keyzers@gmail.com>
2020-07-07 19:15:04 +0100

    Update as per crate publishing guidelines.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,30 @@
 use std::env;
 use std::path;
 
+#[derive(Debug)]
 pub struct ChangeWorkingDirectory {
     previous_directory: path::PathBuf,
 }
 
 impl ChangeWorkingDirectory {
+    /// Store the current working directory, then change it to the supplied path. When the struct
+    /// goes out of scope, the current working directory will be changed back to what it originally
+    /// was.
+    ///
+    /// ```rust
+    /// use std::env;
+    /// use chsh::ChangeWorkingDirectory;
+    ///
+    /// fn main()
+    /// {
+    ///     {
+    ///         let _dir_change = ChangeWorkingDirectory::change(&env::temp_dir());
+    ///         // Do something in the temp dir
+    ///     }
+    ///
+    ///     // _dir_change has gone out of scope, you will be back where you started.
+    /// }
+    /// ```
     pub fn change(new_directory: &impl AsRef<path::Path>) -> Self {
         let current_working_directory = env::current_dir().unwrap();
 
